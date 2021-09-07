a488bf85e7acc5b57936efde4eadd4940bf081c6
TasfiaIslam <tasf1a027@gmail.com>
2021-09-07 18:07:23 +0600

    get project and single post

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import sanityClient from "../client.js"
+
+export interface IProject {
+    id: string;
+    title: string;
+    date: Date;
+    description: Text;
+    projectType: string;
+    link: string;
+    tags: Array<string>;
+  }
 
 const Project = () =>  {
+    const [projectData, setProjectData] = useState<IProject[]>([]);
+
+    useEffect(()=>{
+        sanityClient
+            .fetch(`*[_type == "project"]{
+                title,
+                date,
+                description,
+                projectType,
+                link,
+                tags
+            }`)
+            .then((data) => setProjectData(data))
+            .catch(console.error);
+    }, [])
+
+
     return (
-        <div>
-            <h1>Project</h1>
-        </div>
+        <main className="min-h-screen">
+            <section className="container  mx-auto">
+            <h1 className="text-2xl flex justify-center font-shadow-light text-gray-600">Projects</h1>
+                <h2 className="text-lg text-gray-600  flex justify-center mb-12">Welcome to my projects</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {projectData.map((project) =>(
+                        <article>
+                                <div className="block h-64 relative rounded shadow leading-snug bg-white"
+                                key={project.id}>
+                                    <h1>
+                                    <a href={project.link} target="_blank" rel="noopener noreferrer" >{project.title}</a>
+                                    </h1>
+                                    <p>{project.description}</p>
+                                </div>
+                        </article>
+                    ))}
+                </div>
+            </section>
+        </main>
     )
 }
 
--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router';
+import sanityClient from "../client.js";
+import  imageUrlBuilder  from '@sanity/image-url';
+
+const builder =  imageUrlBuilder(sanityClient);
+function urlFor(source:string){
+    return builder.image(source)
+}
 
 const SinglePost = () =>  {
+    const [singlePost, setSinglePost] = useState(null);
+    const slug = useParams();
+
+    useEffect(()=>{
+        sanityClient
+            .fetch(`*[slug.current == "${slug}"]{
+                title,
+                _id,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    },
+                    alt
+                },
+                body
+            }`)
+            .then((data) => setSinglePost(data[0]))
+            .catch(console.error);
+    }, [])
+
     return (
         <div>
             <h1>Single Post</h1>
